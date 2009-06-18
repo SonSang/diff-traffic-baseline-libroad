1e82644c8d6640ec4d041f58d96c314295766396
Jason Sewall <jasonsewall@gmail.com>
2009-06-17 20:28:40 -0400

    Add partition01 and tests
    Add boost to build system

---
+++ interval-test.cpp
@@ -0,0 +1,64 @@
+#include "partition01.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <boost/foreach.hpp>
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#include <boost/archive/xml_oarchive.hpp>
+#include <boost/archive/xml_iarchive.hpp>
+
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+
+int main(int argc, char *argv[])
+{
+    partition01<std::string> test_int;
+
+    test_int.insert(0.0f, "zero point zero");
+    test_int.insert(0.4f, "zero point four");
+    test_int.insert(0.7f, "zero point seven");
+
+    std::cout << test_int.find(0.1)->second << std::endl;
+    std::cout << test_int.find(0.5)->second << std::endl;
+    std::cout << test_int.find(0.7)->second << std::endl;
+    std::cout << test_int.find(0.8)->second << std::endl;
+
+    BOOST_FOREACH(partition01<std::string>::entry &et, test_int)
+    {
+        et.second += "duck";
+    }
+
+    BOOST_FOREACH(const partition01<std::string>::entry &et, test_int)
+    {
+        std::cout << et.first << ": " << et.second << std::endl;
+    }
+
+    std::cout << test_int.interval_length(test_int.find(0.8)) << std::endl;
+
+    std::string filename("rest-file.xml");
+
+    {
+        std::ofstream ofs(filename.c_str());
+        boost::archive::xml_oarchive oa(ofs);
+        oa << BOOST_SERIALIZATION_NVP(test_int);
+    }
+
+    {
+        std::ifstream ofs(filename.c_str());
+        boost::archive::xml_iarchive ia(ofs);
+
+        partition01<std::string> test_loader;
+        ia >> BOOST_SERIALIZATION_NVP(test_loader);
+
+        BOOST_FOREACH(const partition01<std::string>::entry &et, test_loader)
+        {
+            std::cout << et.first << ": " << et.second << std::endl;
+        }
+    }
+
+    return 0;
+}
---
+++ partition01.hpp
@@ -0,0 +1,108 @@
+#ifndef _PARTITION01_HPP_
+#define _PARTITION01_HPP_
+
+#include <map>
+#include <algorithm>
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/nvp.hpp>
+
+namespace bs = boost::serialization;
+
+template <class T>
+struct partition01 : public std::map<float, T>
+{
+    typedef std::map<float, T>                          base;
+    typedef typename std::map<float, T>::iterator       iterator;
+    typedef typename std::map<float, T>::const_iterator const_iterator;
+    typedef std::pair<const float, T>                   entry;
+
+    partition01() : base()
+    {}
+
+    iterator insert(float x, const T &val)
+    {
+        return base::insert(std::make_pair(x, val)).first;
+    }
+
+    float interval_length(const_iterator c_this_itr) const
+    {
+        const_iterator c_next_itr(c_this_itr);
+        ++c_next_itr;
+
+        if(c_next_itr == this->end())
+            return 1.0f - c_this_itr->first;
+        else
+            return c_next_itr->first - c_this_itr->first;
+    }
+
+    iterator find(float x)
+    {
+        iterator itr = this->upper_bound(x);
+        if(itr == this->begin())
+            return itr;
+        else
+            return --itr;
+    }
+
+    const_iterator find(float x) const
+    {
+        const_iterator itr = this->upper_bound(x);
+        if(itr == this->begin())
+            return itr;
+        else
+            return --itr;
+    }
+
+    iterator begin()
+    {
+        return base::begin();
+    }
+
+    const_iterator begin() const
+    {
+        return base::begin();
+    }
+
+    iterator end()
+    {
+        return base::end();
+    }
+
+    const_iterator end() const
+    {
+        return base::end();
+    }
+
+    iterator operator[](float x)
+    {
+        return find(x);
+    }
+
+    const_iterator operator[](float x) const
+    {
+        return find(x);
+    }
+
+    iterator find_rescale(float x, float &scale)
+    {
+        iterator itr = find(x);
+        scale = (x-itr->first)/interval_length(itr);
+        return itr;
+    }
+
+    const_iterator find_rescale(float x, float &scale) const
+    {
+        const_iterator itr = find(x);
+        scale = (x-itr->first)/interval_length(itr);
+        return itr;
+    }
+
+    template <class Archive>
+    void serialize(Archive &ar, const unsigned int version)
+    {
+        ar & bs::make_nvp("map", bs::base_object<base>(*this));
+    }
+};
+#endif
