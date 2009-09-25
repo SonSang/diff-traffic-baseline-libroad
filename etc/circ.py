65e845297240824269ba5fed6c1f97cf0157a248
Jason Sewall <jasonsewall@gmail.com>
2009-09-25 01:54:30 -0400

    Update circ.py to handle corner cases
    Add debug flag for display

--- etc/circ.py
+++ etc/circ.py
@@ -7,12 +7,10 @@ import matplotlib
 import scipy.linalg
 
 def min_arc(a0, a1):
-    print a0, a1
     a0 = (a0 + 360) % 360
     a1 = (a1 + 360) % 360
     if(a0 > a1):
         a0, a1 = a1, a0
-    print a0, a1, ": ", 360-a1 + a0, ": ", a1 - a0
     if(360-a1 + a0 < a1 - a0):
         a0, a1 = a1, a0
     return a0, a1
@@ -24,27 +22,23 @@ def tan_circ(plist, radius):
     back /= scipy.linalg.norm(back)
     fwd  /= scipy.linalg.norm(fwd)
 
-    det = back[0]*fwd[1] - back[1]*fwd[1]
+    det = back[0]*fwd[1] - back[1]*fwd[0]
 
     beta = radius
-    angle = 0
     if(det < 0):
         back_t = numpy.array([ back[1], -back[0]])
         fwd_t  = numpy.array([ -fwd[1],  fwd[0]])
-    else:
-
+    elif det > 0:
         back_t = numpy.array([-back[1], back[0]])
         fwd_t  = numpy.array([  fwd[1], -fwd[0]])
+    else:
+        return (plist[2], None, None, None, plist[2])
 
     A = numpy.array([[back[0], -fwd[0]],
                      [back[1], -fwd[1]]])
 
-    # B = numpy.array([[ fwd[1]+back[1]],
-    #                  [-fwd[0]-back[0]]])
-
     B = numpy.array([[ fwd_t[0]-back_t[0]],
                      [ fwd_t[1]-back_t[1]]])
-
 
     alpha = scipy.linalg.solve(A, beta*B)
 
@@ -52,31 +46,36 @@ def tan_circ(plist, radius):
     angle1 = math.atan2( -fwd_t[1],  -fwd_t[0])*180.0/math.pi
 
     angle0, angle1 = min_arc(angle0, angle1)
-    print angle0, angle1
 
     center0 = alpha[0] * back + beta*back_t + p
     center1 = alpha[1] * fwd  + beta* fwd_t + p
     return (alpha[0] * back + p, angle0, center0, angle1, alpha[1] * fwd + p)
 
 if __name__ == '__main__':
-    pts = numpy.array([[-5.0, 4.0], [-2.0, 4.0], [-5.0, 2.0], [-2.0, -1.0], [0.0, 1.0], [2.0, 1.0], [2.1, 10]])
+    debug = False
+    pts = numpy.array([[0.0, 4.0], [4.0, 4.0], [4.0, 0.0], [0.0, 0.0], [0.0, 2.0], [-2.0, 2.0], [-2.0, -2.0], [0.0, -2.0], [6.0, -2.0], [6.0, 0.0]])
     lastpt = pts[0]
     pylab.clf()
     ax = pylab.subplot(111)
     for idx in xrange(1, len(pts)-1):
-        rad = 0.5
+        rad = 1.0
         (p0, angle0, center, angle1, p1) = tan_circ(pts[idx-1:idx+2], rad)
+
+        if center == None:
+            continue
 
         linedata = zip(lastpt, p0)
         l0 = matplotlib.lines.Line2D(linedata[0], linedata[1], color='black', linewidth=2.0 )
         ax.add_line(l0)
 
-        linedata = zip(pts[idx-1], pts[idx])
-        l1 = matplotlib.lines.Line2D(linedata[0], linedata[1], color='blue', linewidth=1.0 )
-        ax.add_line(l1)
+        if debug:
+            linedata = zip(pts[idx-1], pts[idx])
+            l1 = matplotlib.lines.Line2D(linedata[0], linedata[1], color='blue', linewidth=1.0 )
+            ax.add_line(l1)
 
-        circle = matplotlib.patches.Circle(center, rad, ec='none', facecolor='#00ff00', fill=True)
-        ax.add_patch(circle)
+        if debug:
+            circle = matplotlib.patches.Circle(center, rad, ec='none', facecolor='#00ff00', fill=True)
+            ax.add_patch(circle)
         circle = matplotlib.patches.Arc(center, 2*rad, 2*rad, 0.0, angle0, angle1, edgecolor='black', lw=2.0, fill=False)
         ax.add_patch(circle)
 
