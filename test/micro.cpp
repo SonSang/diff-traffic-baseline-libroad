3f8d94b5e9e45004af3863c9bb1e8efcebaa3e07
David Wilkie <wilkie@gamma11.(none)>
2010-01-04 12:48:10 -0500

    Main change: The 0 indexed car is actually the last car in the lane, not the first.  This should be changed to use a doubly ended queue anyway.

--- test/micro.cpp
+++ test/micro.cpp
@@ -1,12 +1,9 @@
 #include <libroad/hwm_network.hpp>
-
 #include <utility>
 #include <iostream>
 #include <cmath>
-
 #include <boost/foreach.hpp>
 #include <boost/utility.hpp>
-
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
@@ -79,14 +76,17 @@ public:
         {
             for (int i = 0; i < cars_in_lane[l.first].size(); i++)
             {
-                if (i == 0)
+                if (i == cars_in_lane[l.first].size() - 1)
                 {
                     //Behavior will be determined by the state of the intersection at the end of the lane
-                    cars_in_lane[l.first][i].accel = 0.73;
+                    //ASSUMES car is moving positively
+                    car ghost_car(1, 0, cars_in_lane[l.first][i].lane_length);
+
+                    cars_in_lane[l.first][i].accel = accel_calc(ghost_car, cars_in_lane[l.first][i]);
                 }
                 else
                 {
-                    cars_in_lane[l.first][i].accel = accel_calc(cars_in_lane[l.first][i - 1], cars_in_lane[l.first][i]);
+                    cars_in_lane[l.first][i].accel = accel_calc(cars_in_lane[l.first][i + 1], cars_in_lane[l.first][i]);
                 }
             }
         }
@@ -97,8 +97,6 @@ public:
             {
                 c.dist += c.vel * timestep;
                 c.vel += c.accel * timestep;
-                // cout << c.dist << " is the distance "
-                //      << c.lane_length << " is the lane lenth"<< endl;
                 c.pos = c.dist / c.lane_length; //TODO this does not need to be calculated here, if it is inefficient
             }
         }
@@ -131,7 +129,7 @@ void glWindow::draw(){
         glMatrixMode (GL_PROJECTION);
         glLoadIdentity ();
         gluPerspective(60.0, (GLdouble) w()/(GLdouble) h(), 1.0, -1.0);
-        gluLookAt(0,0,200,0,0,0,0,1,0);
+        gluLookAt(0,0,300,0,0,0,0,1,0);
         glMatrixMode (GL_MODELVIEW);
         glLoadIdentity();
         glEnable (GL_BLEND); glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -209,7 +207,7 @@ int main(int argc, char** argv)
             for (int i = 0; i < cars_per_lane; i++)
             {
                 //TODO Just creating some cars here...
-                sim.cars_in_lane[_lane.second.id].push_back(car(p, 33, lane_lengths[_lane.first]));
+                sim.cars_in_lane[_lane.second.id].push_back(car(p, 15, lane_lengths[_lane.first]));
                 p += 0.1;
             }
     }
