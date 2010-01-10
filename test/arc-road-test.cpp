9fb121d2fd9862540fd7d6493c0f82e566989b67
Jason Sewall <jasonsewall@gmail.com>
2010-01-09 20:23:33 -0500

    Add car-like proxy to help test param setup

--- test/arc-road-test.cpp
+++ test/arc-road-test.cpp
@@ -8,6 +8,66 @@
 #include "arcball.hpp"
 #include "libroad/arc_road.hpp"
 
+static inline tvmet::XprVector<tvmet::VectorConstReference<float, 3>, 3> cvec3f(const float* mem)
+{
+    return tvmet::cvector_ref<float,3>(mem);
+}
+
+static GLuint init_draw_car()
+{
+    static const float LANE_WIDTH = 2.5f;
+    static const float CAR_LENGTH = 4.5f;
+//* This is the position of the car's axle from the FRONT bumper of the car
+    static const float CAR_REAR_AXLE = 3.5f;
+
+    static const float verts[][3] = {{-(CAR_LENGTH-CAR_REAR_AXLE), -0.3f*LANE_WIDTH, 0.0f},  //0
+                                     {              CAR_REAR_AXLE,-0.15f*LANE_WIDTH, 0.0f},  //1
+                                     {              CAR_REAR_AXLE, 0.15f*LANE_WIDTH, 0.0f},  //2
+                                     {-(CAR_LENGTH-CAR_REAR_AXLE),  0.3f*LANE_WIDTH, 0.0f},  //3
+
+                                     {-(CAR_LENGTH-CAR_REAR_AXLE), -0.3f*LANE_WIDTH, 1.5f},  //4
+                                     {              CAR_REAR_AXLE,-0.15f*LANE_WIDTH, 1.3f},  //5
+                                     {              CAR_REAR_AXLE, 0.15f*LANE_WIDTH, 1.3f},  //6
+                                     {-(CAR_LENGTH-CAR_REAR_AXLE),  0.3f*LANE_WIDTH, 1.5f}}; //7
+
+    static const int faces[6][4] = {{ 7, 6, 5, 4}, // bottom
+                                    { 0, 1, 2, 3}, // top
+                                    { 1, 5, 4, 0}, // left side
+                                    { 0, 3, 7, 4}, // back
+                                    { 3, 7, 6, 2}, // right side
+                                    { 5, 6, 2, 1}};// front
+
+    GLuint car_list = glGenLists(1);
+    glPushMatrix();
+    glNewList(car_list, GL_COMPILE);
+    glBegin(GL_QUADS);
+    for(int i = 0; i < 6; ++i)
+    {
+        const vec3f d10 (cvec3f(verts[faces[i][1]]) - cvec3f(verts[faces[i][0]]));
+        const vec3f d20 (cvec3f(verts[faces[i][2]]) - cvec3f(verts[faces[i][0]]));
+        vec3f norm(tvmet::cross(d10, d20));
+        float norm_len = std::sqrt(tvmet::dot(norm, norm));
+        norm /= norm_len;
+        glNormal3f(norm[0], norm[1], norm[2]);
+        for(int j = 0; j < 4; ++j)
+            glVertex3fv(&(verts[faces[i][j]][0]));
+    }
+    glEnd();
+    glEndList();
+    glPopMatrix();
+
+    return car_list;
+}
+
+static void draw_car()
+{
+    static GLuint car_list = 0;
+
+    if(!car_list)
+        car_list = init_draw_car();
+    glCallList(car_list);
+}
+
 static const char vertexShader[] =
 {
     "void main()                                                            \n"
@@ -201,6 +261,32 @@ public:
         glLinkProgram(program);
     }
 
+    void setup_light()
+    {
+        static const GLfloat light_position[] = { 0.0, 100.0, 0.0, 1.0 };
+        static const GLfloat amb_light_rgba[] = { 0.1, 0.1, 0.1, 1.0 };
+        static const GLfloat diff_light_rgba[] = { 0.7, 0.7, 0.7, 1.0 };
+        static const GLfloat spec_light_rgba[] = { 1.0, 1.0, 1.0, 1.0 };
+        static const GLfloat spec_material[] = { 1.0, 1.0, 1.0, 1.0 };
+        static const GLfloat material[] = { 1.0, 1.0, 1.0, 1.0 };
+        static const GLfloat shininess = 100.0;
+
+        glEnable(GL_LIGHTING);
+        glEnable(GL_LIGHT0);
+        glEnable(GL_COLOR_MATERIAL);
+        glPushMatrix();
+        glLoadIdentity();
+        glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+        glPopMatrix();
+        glLightfv(GL_LIGHT0, GL_AMBIENT, amb_light_rgba );
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, diff_light_rgba );
+        glLightfv(GL_LIGHT0, GL_SPECULAR, spec_light_rgba );
+        glMaterialfv( GL_FRONT, GL_AMBIENT, material );
+        glMaterialfv( GL_FRONT, GL_DIFFUSE, material );
+        glMaterialfv( GL_FRONT, GL_SPECULAR, spec_material );
+        glMaterialfv( GL_FRONT, GL_SHININESS, &shininess);
+    }
+
     void make_texture()
     {
         int resolution = 32;
@@ -252,6 +338,8 @@ public:
 
             if(!texture)
                 make_texture();
+
+            setup_light();
         }
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
@@ -276,9 +364,9 @@ public:
             glEnd();
         }
 
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         if(ar)
         {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
             glColor3f(1.0, 0.0, 0.0);
             glBegin(GL_LINE_STRIP);
             BOOST_FOREACH(const vec3f &p, ar->extract_line(low_bnd, low_res))
@@ -295,17 +383,21 @@ public:
             glEnd();
 
             vec3f pos = ar->point(car_pos, low_bnd);
-            std::cout << "t: " << car_pos << " offset: " << low_bnd << " pos: " << pos << std::endl;
             glColor3f(1.0, 1.0, 0.0);
+
+            glDisable(GL_BLEND);
+            glEnable(GL_LIGHTING);
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
             glPushMatrix();
             glTranslatef(pos[0], pos[1], pos[2]);
-            glBegin(GL_QUADS);
-            glVertex2f(-0.5, -0.5);
-            glVertex2f( 0.5, -0.5);
-            glVertex2f( 0.5,  0.5);
-            glVertex2f(-0.5,  0.5);
-            glEnd();
+            //glRotatef(me.orientation*180.0f*M_1_PI, 0.0f, 0.0f, 1.0f);
+            draw_car();
             glPopMatrix();
+
+            glEnable(GL_BLEND);
+            glDisable(GL_LIGHTING);
+
         }
 
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
@@ -328,6 +420,8 @@ public:
         glActiveTextureARB(GL_TEXTURE0_ARB);
         glBindTexture(GL_TEXTURE_2D, texture);
 
+        glDepthMask(GL_FALSE);
+
         glBegin(GL_POINTS);
         for(size_t i = 0; i < pr->points_.size(); ++i)
         {
@@ -342,6 +436,8 @@ public:
         glUseProgram(0);
         glDisable(GL_POINT_SPRITE_ARB);
         glDisable(GL_TEXTURE_2D);
+
+        glDepthMask(GL_TRUE);
 
         glFlush();
         glFinish();
