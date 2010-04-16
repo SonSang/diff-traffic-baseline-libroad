4d4ab61aa69a60759cfc9049416de71266977b1d
Jason Sewall <jasonsewall@gmail.com>
2010-04-16 11:58:17 -0400

    Add dep on FreeImage so we can generate lane textures

--- test/mesh-extract-test.cpp
+++ test/mesh-extract-test.cpp
@@ -1,6 +1,7 @@
 #include "libroad/arc_road.hpp"
 
 #include <fstream>
+#include <FreeImage.h>
 
 void dump_obj(std::ostream &out, const std::vector<vertex> &verts,
               std::vector<vec3u> &faces)
@@ -23,9 +24,49 @@ void dump_obj(std::ostream &out, const std::vector<vertex> &verts,
     }
 };
 
+static const float lane_width      = 2.5f;
+static const float shoulder_width  = 3.5f;
+static const float line_width      = 0.125;
+static const float line_sep_width  = 0.125;
+static const float line_length     = 3.0f;
+static const float line_gap_length = 9.0f;
+
+void create_lane_image(bool lshoulder, int llanes, int rlanes, bool rshoulder)
+{
+    const float total_length = (lshoulder + rshoulder)*shoulder_width
+                                      + lane_width * (llanes + rlanes);
+
+    const int xres = static_cast<int>(std::ceil(total_length/line_sep_width));
+    const int yres = static_cast<int>(std::ceil((line_gap_length+line_length)/line_length));
+
+    FIBITMAP *im = FreeImage_Allocate(100, 100, 32, FI_RGBA_RED_MASK, FI_RGBA_GREEN_MASK, FI_RGBA_BLUE_MASK);
+    assert(im);
+    const int bytespp = FreeImage_GetLine(im) / FreeImage_GetWidth(im);
+
+    for(unsigned int y = 0; y < FreeImage_GetHeight(im); y++)
+    {
+        BYTE *bits = FreeImage_GetScanLine(im, y);
+        for(unsigned int x = 0; x <  FreeImage_GetWidth(im); x++)
+        {
+            bits[FI_RGBA_RED]   = 255;
+            bits[FI_RGBA_GREEN] = 0;
+            bits[FI_RGBA_BLUE] =  0;
+            bits[FI_RGBA_ALPHA] = 255;
+            // jump to next pixel
+            bits += bytespp;
+        }
+    }
+
+    FreeImage_Save(FIF_PNG, im, "test.png");
+
+    FreeImage_Unload(im);
+}
+
 int main(int argc, char *argv[])
 {
     std::cerr << libroad_package_string() << std::endl;
+
+    create_lane_image(false, 1, 1, false);
 
     arc_road ar;
     ar.points_.push_back(vec3f(000, 400, 0.0));
