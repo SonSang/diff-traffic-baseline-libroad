5de8856ff232cab83240636d742bcf3bb37769f5
Jason Sewall <jasonsewall@gmail.com>
2010-04-16 12:33:08 -0400

    Add basic lane texture maker

--- test/mesh-extract-test.cpp
+++ test/mesh-extract-test.cpp
@@ -25,48 +25,128 @@ void dump_obj(std::ostream &out, const std::vector<vertex> &verts,
 };
 
 static const float lane_width      = 2.5f;
-static const float shoulder_width  = 3.5f;
+static const float shoulder_width  = 2.0f;
 static const float line_width      = 0.125;
 static const float line_sep_width  = 0.125;
 static const float line_length     = 3.0f;
 static const float line_gap_length = 9.0f;
 
-void create_lane_image(bool lshoulder, int llanes, int rlanes, bool rshoulder)
+void set_dashed(FIBITMAP *im, const int y, const int bytespp)
+{
+    BYTE *bits = FreeImage_GetScanLine(im, y);
+    for(unsigned int x = static_cast<unsigned int>(std::floor(line_gap_length/(line_gap_length+line_length)*FreeImage_GetWidth(im))); x < FreeImage_GetWidth(im); x++)
+    {
+        bits[FI_RGBA_RED]   = 255;
+        bits[FI_RGBA_GREEN] = 255;
+        bits[FI_RGBA_BLUE] =  255;
+        bits[FI_RGBA_ALPHA] = 255;
+        // jump to next pixel
+        bits += bytespp;
+    }
+}
+
+void set_solid_white(FIBITMAP *im, const int y, const int bytespp)
+{
+    BYTE *bits = FreeImage_GetScanLine(im, y);
+    for(unsigned int x = 0; x <  FreeImage_GetWidth(im); x++)
+    {
+        bits[FI_RGBA_RED]   = 255;
+        bits[FI_RGBA_GREEN] = 255;
+        bits[FI_RGBA_BLUE] =  255;
+        bits[FI_RGBA_ALPHA] = 255;
+        // jump to next pixel
+        bits += bytespp;
+    }
+}
+
+void set_solid_yellow(FIBITMAP *im, const int y, const int bytespp)
+{
+    BYTE *bits = FreeImage_GetScanLine(im, y);
+    for(unsigned int x = 0; x < FreeImage_GetWidth(im); x++)
+    {
+        bits[FI_RGBA_RED]   = 0xFF;
+        bits[FI_RGBA_GREEN] = 0xe7;
+        bits[FI_RGBA_BLUE] =  0x00;
+        bits[FI_RGBA_ALPHA] = 255;
+        // jump to next pixel
+        bits += bytespp;
+    }
+}
+
+FIBITMAP *create_lane_image(bool lshoulder, int llanes, int rlanes, bool rshoulder)
 {
     const float total_length = (lshoulder + rshoulder)*shoulder_width
-                                      + lane_width * (llanes + rlanes);
+                               + lane_width * (llanes + rlanes);
 
-    const int xres = static_cast<int>(std::ceil(total_length/line_sep_width));
-    const int yres = static_cast<int>(std::ceil((line_gap_length+line_length)/line_length));
+    const int xres = static_cast<int>(std::ceil((line_gap_length+line_length)/line_length));
+    const int yres = static_cast<int>(std::ceil(total_length/line_sep_width));
 
-    FIBITMAP *im = FreeImage_Allocate(100, 100, 32, FI_RGBA_RED_MASK, FI_RGBA_GREEN_MASK, FI_RGBA_BLUE_MASK);
+    FIBITMAP *im = FreeImage_Allocate(xres, yres, 32, FI_RGBA_RED_MASK, FI_RGBA_GREEN_MASK, FI_RGBA_BLUE_MASK);
     assert(im);
     const int bytespp = FreeImage_GetLine(im) / FreeImage_GetWidth(im);
 
     for(unsigned int y = 0; y < FreeImage_GetHeight(im); y++)
     {
         BYTE *bits = FreeImage_GetScanLine(im, y);
-        for(unsigned int x = 0; x <  FreeImage_GetWidth(im); x++)
+        for(unsigned int x = 0; x < FreeImage_GetWidth(im); x++)
         {
-            bits[FI_RGBA_RED]   = 255;
-            bits[FI_RGBA_GREEN] = 0;
-            bits[FI_RGBA_BLUE] =  0;
             bits[FI_RGBA_ALPHA] = 255;
-            // jump to next pixel
             bits += bytespp;
         }
     }
 
-    FreeImage_Save(FIF_PNG, im, "test.png");
+    float current = 0.0f;
+    if(lshoulder)
+    {
+        current += (shoulder_width-line_width*0.5);
+        set_solid_white(im, current/total_length*FreeImage_GetHeight(im), bytespp);
+        current += line_width*0.5f;
+    }
 
-    FreeImage_Unload(im);
+    for(int i = llanes; i > 1; --i)
+    {
+        current += (lane_width-line_width*0.5);
+        set_dashed(im, current/total_length*FreeImage_GetHeight(im), bytespp);
+        current += line_width*0.5f;
+    }
+
+    if(llanes > 0)
+    {
+        current += (lane_width-line_width);
+        set_solid_yellow(im, current/total_length*FreeImage_GetHeight(im), bytespp);
+        current += line_width;
+    }
+
+    if(rlanes > 0)
+    {
+        current += line_width;
+        set_solid_yellow(im, current/total_length*FreeImage_GetHeight(im), bytespp);
+        current += lane_width - line_width;
+    }
+
+    for(int i = 1; i < rlanes; ++i)
+    {
+        current -= line_width*0.5;
+        set_dashed(im, current/total_length*FreeImage_GetHeight(im), bytespp);
+        current += lane_width - line_width*0.5f;
+    }
+
+    if(rshoulder)
+    {
+        current -= line_width*0.5;
+        set_solid_white(im, current/total_length*FreeImage_GetHeight(im), bytespp);
+    }
+
+    return im;
 }
 
 int main(int argc, char *argv[])
 {
     std::cerr << libroad_package_string() << std::endl;
 
-    create_lane_image(false, 1, 1, false);
+    FIBITMAP *im = create_lane_image(true, 2, 1, true);
+    FreeImage_Save(FIF_PNG, im, "test.png");
+    FreeImage_Unload(im);
 
     arc_road ar;
     ar.points_.push_back(vec3f(000, 400, 0.0));
