5186ef475fb834cef95c5216c9d443efa9fb8764
Jason Sewall <jasonsewall@gmail.com>
2010-04-19 14:26:05 -0400

    Add code to automatically write out material library in mesh-extract

--- test/mesh-extract-test.cpp
+++ test/mesh-extract-test.cpp
@@ -153,6 +153,9 @@ FIBITMAP *create_lane_image(bool lshoulder, int llanes, int rlanes, bool rshould
 
 struct lane_tex
 {
+    typedef std::map<const int, const std::string> str_map;
+
+
     lane_tex(const std::string &r) : root(r) {}
 
     static const int max_lanes = 1024;
@@ -169,13 +172,13 @@ struct lane_tex
         return (llanes * 1024 + rlanes) * 1024 +  lshoulder *2 + rshoulder;
     }
 
-    std::string tex_string(bool lshoulder, int llanes, int rlanes, bool rshoulder)
+    static std::string tex_string(bool lshoulder, int llanes, int rlanes, bool rshoulder)
     {
         canon(lshoulder, llanes, rlanes, rshoulder);
         return boost::str(boost::format("ls%d-ll%d-rl%d-rs%d") % lshoulder % llanes % rlanes % rshoulder);
     }
 
-    std::string tex_path(const std::string &s)
+    std::string tex_path(const std::string &s) const
     {
         return boost::str(boost::format("%s%s.png") % root % s);
     }
@@ -183,7 +186,7 @@ struct lane_tex
     const std::string write_tex(const bool lshoulder, const int llanes, const int rlanes, const bool rshoulder)
     {
         const int idx(hash_index(lshoulder, llanes, rlanes, rshoulder));
-        std::map<const int, const std::string>::iterator c(texes.find(idx));
+        const str_map::iterator c(texes.find(idx));
         if(c == texes.end())
         {
             FIBITMAP *im = create_lane_image(lshoulder, llanes, rlanes, rshoulder);
@@ -194,6 +197,29 @@ struct lane_tex
             return fi;
         }
         return c->second;
+    }
+
+    void write_mtl(std::ostream &o,
+                   const std::string &ts) const
+    {
+        o << boost::str(boost::format("newmtl %s\n"
+                                      "ns 96.078431\n"
+                                      "ka 0.0  0.0  0.0\n"
+                                      "kd 0.64 0.64 0.64\n"
+                                      "ks 0.5  0.5  0.5\n"
+                                      "ni 1.0\n"
+                                      "d  1.0\n"
+                                      "map_kd %s\n") % ts % tex_path(ts));
+    }
+
+    void write_mtllib(const std::string &fname) const
+    {
+        std::ofstream o(fname.c_str());
+
+        BOOST_FOREACH(const str_map::value_type &i, texes)
+        {
+            write_mtl(o, i.second);
+        }
     }
 
     std::map<const int, const std::string> texes;
@@ -361,6 +387,8 @@ int main(int argc, char *argv[])
             ++re_c;
         }
     }
+
+    ltb.write_mtllib("road.mtl");
 
     return 0;
 }
