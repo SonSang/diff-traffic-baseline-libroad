a132fa6aa18b82102a39b718c52b92ec8164f23c
Jason Sewall <jasonsewall@gmail.com>
2010-04-22 00:18:15 -0400

    Add code to displace polylines (with splits) based on input polylnes and images

--- test/displace-polylines.cpp
+++ test/displace-polylines.cpp
@@ -1,6 +1,7 @@
 #include "Magick++.h"
 #include "libroad/hwm_network.hpp"
 #include <iostream>
+#include <fstream>
 
 struct im_heightfield
 {
@@ -43,7 +44,71 @@ struct im_heightfield
                                (nw*(1-remainder[0]) + ne*remainder[0])*remainder[1]);
     }
 
-    void make_mesh(std::vector<vertex> &vrts, std::vector<vec3u> &faces)
+    vec3f vlookup(const vec2f &xy) const
+    {
+        return vec3f(xy[0], xy[1], lookup(xy));
+    }
+
+    vec3f vlookup(const vec3f &xy) const
+    {
+        return vec3f(xy[0], xy[1], lookup(vec2f(xy[0], xy[1])));
+    }
+
+    std::vector<vec3f> displace_polyline(const std::vector<vec3f> &poly, const float fac_limit) const
+    {
+        std::vector<vec3f>                 res;
+        std::vector<vec3f>                 to_add;
+        std::vector<vec3f>::const_iterator input(poly.begin());
+
+        res.push_back(vlookup(*input++));
+        to_add.push_back(vlookup(*input++));
+
+        vec3f split;
+        while(true)
+        {
+            while(!to_add.empty())
+            {
+                while(max_diff(split, res.back(), to_add.back(), fac_limit))
+                {
+                    to_add.push_back(split);
+                }
+                res.push_back(to_add.back());
+                to_add.pop_back();
+            }
+
+            if(input == poly.end())
+                return res;
+
+            to_add.push_back(vlookup(*input++));
+        }
+    }
+
+    bool max_diff(vec3f &best, const vec3f &v0, const vec3f &v1, const float fac_limit) const
+    {
+        const vec3f dvec(v1-v0);
+        const float len(length(dvec));
+        const float inv_len = 1.0f/len;
+        const vec3f nvec(dvec*inv_len);
+
+        const float dt       = 0.5*std::min(spacing[0], spacing[1])*inv_len;
+        float       best_fac = fac_limit;
+        bool        best_set = false;
+        for(float t = dt; t < len; t += dt)
+        {
+            const vec3f mid(v0 + nvec*t);
+            const vec3f cand(vlookup(mid));
+            float       cand_fac(std::abs(cand[2] - mid[2])*inv_len);
+            if(cand_fac > best_fac)
+            {
+                best_set = true;
+                best     = cand;
+                best_fac = cand_fac;
+            }
+        }
+        return best_set;
+    };
+
+    void make_mesh(std::vector<vertex> &vrts, std::vector<vec3u> &faces) const
     {
         for(int j = 0; j < dim[1]; ++j)
         {
@@ -51,7 +116,7 @@ struct im_heightfield
             {
                 const vec3f pos(i*spacing[0] + origin[0],
                                 j*spacing[1] + origin[1],
-                                pix[i + j * dim[0]]);
+                                zbase + zscale*pix[i + j * dim[0]]);
 
                 const vec2f uv(i/static_cast<float>(dim[0]-1),
                                j/static_cast<float>(dim[1]-1));
@@ -145,16 +210,30 @@ int main(int argc, char *argv[])
     im.quantize();
 
     im.flip();
-    im_heightfield ih(im, 0, 1);
+    im_heightfield ih(im, 0, 24);
 
-    ih.origin = vec2f(0.0);
-    ih.spacing = vec2f(0.1);
+    ih.origin = vec2f(-50.0);
+    ih.spacing = vec2f(1);
 
     std::vector<vertex> vrts;
     std::vector<vec3u>  fcs;
-
     ih.make_mesh(vrts, fcs);
-    dump_obj(std::cout, "test", "test", vrts, fcs);
+    std::ofstream os("im.obj");
+    dump_obj(os, "test", "test", vrts, fcs);
+
+    std::vector<vec3f> poly;
+    poly.push_back(vec3f(0.0));
+    poly.push_back(vec3f(0.0, 5.0, 0.0));
+    poly.push_back(vec3f(10.0));
+    poly = ih.displace_polyline(poly, 0.1);
+
+    vrts.clear();
+    fcs.clear();
+    arc_road ar;
+    ar.initialize_from_polyline(0.0f, poly);
+    ar.make_mesh(vrts, fcs, vec2f(0, 1), vec2f(-0.1, 0.1), 0.01);
+
+    dump_obj(os, "poly", "poly", vrts, fcs);
 
     return 0;
 }
