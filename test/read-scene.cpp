7c3695bb88e8458274efcdaa0128d5eba004ad70
Jason Sewall <jasonsewall@gmail.com>
2010-05-11 06:37:51 -0400

    Allow 32-bit 'raw' images in read-scene

--- test/read-scene.cpp
+++ test/read-scene.cpp
@@ -5,32 +5,31 @@
 int main(int argc, char *argv[])
 {
     std::cout << libroad_package_string() << std::endl;
-    if(argc != 9)
+    if(argc != 11)
     {
-        std::cerr << "Usage: " << argv[0] << " <input scene> <heightfield> <spacing> <originx> <originy> <zbase> <zscale> <output scene>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <input scene> <heightfield> <dimx> <dimy> <spacing> <originx> <originy> <zbase> <zscale> <output scene>" << std::endl;
         return 1;
     }
 
     std::vector<obj_record> objs(xml_read_scene(argv[1]));
 
-    // Magick::Image im(argv[2]);
-    // const vec2i dim(im.columns(), im.rows());
-    // float *pix = new float[dim[0]*dim[1]];
-    // im.write(0, 0, dim[1], dim[0], "R", Magick::FloatPixel, pix);
+    FILE *fp = fopen(argv[2], "rb");
+    const vec2i dim(boost::lexical_cast<float>(argv[3]), boost::lexical_cast<float>(argv[4]));
+    float *pix = new float[dim[0]*dim[1]];
+    fread(pix, sizeof(float), dim[0]*dim[1], fp);
+    im_heightfield ih(pix, dim, boost::lexical_cast<float>(argv[8]), boost::lexical_cast<float>(argv[9]));
+    ih.origin  = vec2f(boost::lexical_cast<float>(argv[6]), boost::lexical_cast<float>(argv[7]));
+    ih.spacing = vec2f(boost::lexical_cast<float>(argv[5]), boost::lexical_cast<float>(argv[5]));
 
-    // im_heightfield ih(pix, dim, boost::lexical_cast<float>(argv[6]), boost::lexical_cast<float>(argv[7]));
-    // ih.origin  = vec2f(boost::lexical_cast<float>(argv[4]), boost::lexical_cast<float>(argv[5]));
-    // ih.spacing = vec2f(boost::lexical_cast<float>(argv[3]), boost::lexical_cast<float>(argv[3]));
+    delete[] pix;
 
-    // delete[] pix;
+    BOOST_FOREACH(obj_record &obj_r, objs)
+    {
+        float dz = ih.project_bb(obj_r.box);
+        obj_r.matrix(2, 3) -= dz;
+    }
 
-    // BOOST_FOREACH(obj_record &obj_r, objs)
-    // {
-    //     float dz = ih.project_bb(obj_r.box);
-    //     obj_r.matrix(2, 3) -= dz;
-    // }
-
-    xml_write_scene(argv[8], objs);
+    xml_write_scene(argv[10], objs);
 
     return 0;
 }
